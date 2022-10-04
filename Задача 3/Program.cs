ec3ca5915f2e63abf6f74e1c2002ff28eec0bce2
AlexanderD <avdzyaduk@gmail.com>
2022-10-04 21:15:31 +0300

    Задача 3

---
+++ Задача 3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+int num1 = new Random().Next(100, 1000);
+int res = num % num1;
+if (res == 0)
+{
+    Console.WriteLine($"Число {num} кратное {num1}");
+}
+else 
+{
+    Console.WriteLine($"Число {num} не кратное {num1} остаток - {res}");
+}
