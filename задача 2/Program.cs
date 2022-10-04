6838f649bd701cb16e976e64e4dab779e67832f8
AlexanderD <avdzyaduk@gmail.com>
2022-10-04 20:48:16 +0300

    зададча 2

---
+++ задача 2/Program.cs
@@ -0,0 +1,7 @@
+﻿// задача 9
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+int a1 = num / 100;
+int a2 = num % 10;
+Console.WriteLine($"первое и после цифры числа {num} -> {a1} {a2}");
