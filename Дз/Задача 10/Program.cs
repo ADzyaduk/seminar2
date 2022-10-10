cd8f3f560254d7d535df2591c8dc4daaf29486dd
Alex Dz <avdzyaduk@gmail.com>
2022-10-10 12:24:24 +0300

    задача 10

--- Дз/Задача 10/Program.cs
+++ Дз/Задача 10/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+   456 -> 5
+   782 -> 8
+   918 -> 1 */
+
+   Console.Clear();
+
+   Console.WriteLine("Введите 3-ёх значное число:");
+   int number = Convert.ToInt32(Console.ReadLine());
+
+   if (number < 100 || number > 999)
+   {
+      Console.WriteLine("Ошибка! Число не 3-х значное:");
+   }
+   else 
+   {
+     number = (number/10)%10;
+     Console.WriteLine(number);
+   }
